2b1c5b2354e6c78e8cda19546acdae93834ce024
matthewjordanau@gmail.com <matthewjordanau@gmail.com>
2019-05-03 14:59:28 -0700

    Exercise 3 Completed. Completed JS.

--- react.js
+++ react.js
@@ -17,7 +17,7 @@ export default class Sentient extends Component {
   }
 }
 
-// ReactDOM.render(<Sentient />, document.getElementById('App'))
+ReactDOM.render(<Sentient />, document.getElementById('App'))
 
 // 2. Practice With Loops: In React, we often use the map function to iterate over an array held in state. To get more practice with this, write a vanilla javascript for loop that outputs the same thing as the map function below.
 
@@ -36,8 +36,7 @@ for (let i=0, i<stuffArray.length; i++){
 
 // 3. Basic sorting: Find the cheapest price -- you are given a list of prices and need to find the signle lowest price from the array. Write a function that takes in an array of numbers, and returns the lowest price
 
-var steal = function(){
-
-  // your logic here
-
+var steal = function(arr){
+  arr.sort((a, b)=>{return a - b})
+  return arr[0]
 }
